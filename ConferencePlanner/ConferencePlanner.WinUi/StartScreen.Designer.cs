54b43dcaede1adcebe8fd8ba03a167e77793c77d
Ileana Zavoianu <ileana.zavoianu@totalsoft.ro>
2020-09-15 14:49:08 +0300

    some designe

--- ConferencePlanner/ConferencePlanner.WinUi/StartScreen.Designer.cs
+++ ConferencePlanner/ConferencePlanner.WinUi/StartScreen.Designer.cs
@@ -33,9 +33,7 @@
             this.txtButton = new System.Windows.Forms.TextBox();
             this.submitButton = new ConferencePlanner.WinUi.ButonCircular();
             this.errorProviderEmailText = new System.Windows.Forms.ErrorProvider(this.components);
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProviderEmailText)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtButton
@@ -43,7 +41,7 @@
             this.txtButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtButton.Location = new System.Drawing.Point(84, 83);
+            this.txtButton.Location = new System.Drawing.Point(82, 67);
             this.txtButton.Name = "txtButton";
             this.txtButton.PlaceholderText = "                Insert your email";
             this.txtButton.Size = new System.Drawing.Size(238, 23);
@@ -57,7 +55,7 @@
             this.submitButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("submitButton.BackgroundImage")));
             this.submitButton.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.submitButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.submitButton.Location = new System.Drawing.Point(146, 112);
+            this.submitButton.Location = new System.Drawing.Point(145, 96);
             this.submitButton.Name = "submitButton";
             this.submitButton.Size = new System.Drawing.Size(104, 29);
             this.submitButton.TabIndex = 4;
@@ -69,16 +67,6 @@
             // 
             this.errorProviderEmailText.ContainerControl = this;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1, -1);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(87, 78);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
             // StartScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -86,7 +74,6 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(394, 219);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.submitButton);
             this.Controls.Add(this.txtButton);
             this.Name = "StartScreen";
@@ -95,7 +82,6 @@
             this.Text = "Event Skills";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StartScreen_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.errorProviderEmailText)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,6 +93,5 @@
         private ButonCircular submitButton1;
         private ButonCircular submitButton;
         private System.Windows.Forms.ErrorProvider errorProviderEmailText;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
